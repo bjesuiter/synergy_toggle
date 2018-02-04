af65336bdc8ba4935f486341ca1fd46fc1a8051f
Benjamin Jesuiter <bjesuiter@gmail.com>
2018-02-04 14:44:31 +0100

    enables sudo again (not the execution problem)

--- src/disable_synergy.sh
+++ src/disable_synergy.sh
@@ -2,6 +2,4 @@
 
 # Option for enabling and disabling the Service will come in 2018
 launchctl unload /Library/LaunchAgents/com.symless.synergy.synergy-service.plist
-
-# deactivated because this does not work in final mac app because of sudo
-#sudo killall synergy-core
+sudo killall synergy-core
