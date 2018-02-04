a1a61cf376561d52d87c4cdc3785f852003be643
Benjamin Jesuiter <bjesuiter@gmail.com>
2018-02-04 14:39:22 +0100

    deactivates sudo command in disable_snyergy with comment

--- src/disable_synergy.sh
+++ src/disable_synergy.sh
@@ -3,4 +3,5 @@
 # Option for enabling and disabling the Service will come in 2018
 launchctl unload /Library/LaunchAgents/com.symless.synergy.synergy-service.plist
 
-sudo killall synergy-core
+# deactivated because this does not work in final mac app because of sudo
+#sudo killall synergy-core
